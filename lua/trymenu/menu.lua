2269c126c5a61e7897b2b5d412c08f5afb08898c
spout <snoopyxdy@gmail.com>
2014-03-21 09:14:27 +0800

    add deal

--- lua/trymenu/menu.lua
+++ lua/trymenu/menu.lua
@@ -1,3 +1,5 @@
+local enterTime = tonumber(os.time()) -- 进入时候时间戳，用来判断是否超时
+
 local Mysql_Class = require "mysql_class2"["Mysql_CLass"] --数据库db类
 
 local args = ngx.req.get_uri_args() or {}
@@ -15,20 +17,26 @@ end
 
 if args.noencode then
 
-	return ngx.say(json)
+	ngx.say(json)
+else
 
+	local XorKey = 4
+	local s = ngx.encode_base64(json)
+
+	local stable={}
+	for i=1,#s do
+		table.insert(stable, string.char(bit.bxor(string.byte(s, i),XorKey)))	
+	end
+	local s2 = table.concat(stable)
+
+	-- ngx.header["Content-Type"] = 'application/json; charset=UTF-8';
+
+	ngx.say(s2)
 end
 
-local XorKey = 4
-local s = ngx.encode_base64(json)
+local resTime = tonumber(os.time()) -- 响应后时间戳
 
-local stable={}
-for i=1,#s do
-	table.insert(stable, string.char(bit.bxor(string.byte(s, i),XorKey)))	
+local dealTime = resTime - enterTime
+if dealTime > 5 then
+	ngx.log(ngx.ERR, "deal request too long :" ..tostring(dealTime)) --出错记录错误日志	
 end
-local s2 = table.concat(stable)
-
--- ngx.header["Content-Type"] = 'application/json; charset=UTF-8';
-
-ngx.say(s2)
-
